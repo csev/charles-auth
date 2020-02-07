d382d25ecef57ecd18c026c06670fbecad476665
Paul Severance <paul@sugarush.io>
2020-02-07 18:30:25 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+__author__ = 'Paul Severance'
+
+from setuptools import setup
+
+setup(
+    name='charles-auth',
+    version='0.0.1',
+    author='Paul Severance',
+    author_email='paul.severance@gmail.com',
+    url='https://github.com/sugarush/charles-auth',
+    packages=[
+        'charles_auth'
+    ],
+    description='Dr Chuck\'s authentication method.'
+)
